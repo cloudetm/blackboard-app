da7b52af6dc9403e775e2cb57b38d781e3ceb5e8
Christopher Licata <chris@bizmerlin.com>
2015-11-14 17:48:05 -0500

    Created API and DAO Package Structure

---
+++ src/com/blackboard/api/dao/BlackboardDAO.java
@@ -0,0 +1,8 @@
+package com.blackboard.api.dao;
+
+/**
+ * Created by ChristopherLicata on 11/14/15.
+ */
+public interface BlackboardDAO
+{
+}
---
+++ src/com/blackboard/api/dao/impl/MySQLDAO.java
@@ -0,0 +1,8 @@
+package com.blackboard.api.dao.impl;
+
+/**
+ * Created by ChristopherLicata on 11/14/15.
+ */
+public class MySQLDAO
+{
+}
