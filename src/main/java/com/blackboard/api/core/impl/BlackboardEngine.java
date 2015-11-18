ea1cda07f1f48b811d7c0d3636b2145e01daa0a7
Phil Lopreiato <plnyyanks@gmail.com>
2015-11-17 23:15:15 -0500

    Implement BlackboardEngine calculations
    Calculates average, weighted average, and GPA. Also includes tests (plus
    a dependency on mockito - http://mockito.org)

--- src/main/java/com/blackboard/api/core/impl/BlackboardEngine.java
+++ src/main/java/com/blackboard/api/core/impl/BlackboardEngine.java
@@ -1,6 +1,8 @@
 package com.blackboard.api.core.impl;
 
+import com.blackboard.api.core.model.Course;
 import com.blackboard.api.core.model.Grade;
+import com.blackboard.api.core.model.Submission;
 import com.blackboard.api.core.model.Transcript;
 
 import java.util.List;
@@ -16,21 +18,53 @@ import java.util.List;
 public class BlackboardEngine
 {
 
+	/**
+	 * Calculate the averages of a list of {@link Grade} objects
+	 * @param gradeList Grades to average
+	 * @return Resulting average
+	 */
 	public static double calculateAverage(List<Grade> gradeList)
 	{
-		return 0;
+		return gradeList.stream()
+						.mapToInt(Grade::getScore)
+						.average()
+						.orElse(0);
 	}
 
 
+	/**
+	 * Compute a weighted average of {@link Grade} objects by the weight of their
+	 * associated {@link Submission}
+	 * @param gradeList Grades to average
+	 * @return Resulting weighted average
+	 */
 	public static double calculateWeightedAverage(List<Grade> gradeList)
 	{
-		return 0;
+		return gradeList.stream()
+						.mapToDouble((grade -> grade.getScore() * grade.getSubmission().getWeight()))
+						.sum();
 	}
 
 
+	/**
+	 * Compute a student's GPA from a list of their completed courses
+	 * Uses the transcript's associated {@link Course} and {@link Grade} for credit weight / result
+	 *
+	 * Returns a percent of total points earned (100 * number of credits per course)
+	 * @param transcripts Transcripts to average
+	 * @return Resulting weighted average
+     */
 	public static double calculateGPA(List<Transcript> transcripts)
 	{
-		return 0;
+		double totalPoints = 0;
+		double earnedPoints = 0;
+		for (Transcript transcript : transcripts) {
+			int credits = transcript.getCourse().getCredits();
+			int grade = transcript.getGrade().getScore();
+			totalPoints += (credits * 100);
+			earnedPoints += (credits * grade);
+		}
+		return transcripts.size() > 0 ? (earnedPoints / totalPoints) * 100 : 0;
 	}
 
 }
